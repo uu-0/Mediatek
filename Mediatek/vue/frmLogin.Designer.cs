f025dcaa4d4eb7063b456cdc016a5a76c8bdb338
louise <louisepop14@gmail.com>
2022-05-29 00:39:51 +0200

    Ajout image dans frmLogin

--- Mediatek/vue/frmLogin.Designer.cs
+++ Mediatek/vue/frmLogin.Designer.cs
@@ -30,13 +30,15 @@ namespace Mediatek.vue
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lbLogin = new System.Windows.Forms.Label();
-            this.lbMdp = new System.Windows.Forms.Label();
-            this.checkMdp = new System.Windows.Forms.CheckBox();
-            this.txtLogin = new System.Windows.Forms.TextBox();
-            this.txtMdp = new System.Windows.Forms.TextBox();
             this.btnConnect = new System.Windows.Forms.Button();
+            this.txtMdp = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.checkMdp = new System.Windows.Forms.CheckBox();
+            this.lbMdp = new System.Windows.Forms.Label();
+            this.lbLogin = new System.Windows.Forms.Label();
+            this.picLogin = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogin)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -54,51 +56,6 @@ namespace Mediatek.vue
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
-            // lbLogin
-            // 
-            this.lbLogin.AutoSize = true;
-            this.lbLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbLogin.Location = new System.Drawing.Point(6, 30);
-            this.lbLogin.Name = "lbLogin";
-            this.lbLogin.Size = new System.Drawing.Size(130, 20);
-            this.lbLogin.TabIndex = 0;
-            this.lbLogin.Text = "Nom d\'utilisateur ";
-            // 
-            // lbMdp
-            // 
-            this.lbMdp.AutoSize = true;
-            this.lbMdp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbMdp.Location = new System.Drawing.Point(6, 97);
-            this.lbMdp.Name = "lbMdp";
-            this.lbMdp.Size = new System.Drawing.Size(105, 20);
-            this.lbMdp.TabIndex = 1;
-            this.lbMdp.Text = "Mot de passe";
-            // 
-            // checkMdp
-            // 
-            this.checkMdp.AutoSize = true;
-            this.checkMdp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkMdp.Location = new System.Drawing.Point(10, 146);
-            this.checkMdp.Name = "checkMdp";
-            this.checkMdp.Size = new System.Drawing.Size(185, 22);
-            this.checkMdp.TabIndex = 2;
-            this.checkMdp.Text = "Afficher le mot de passe";
-            this.checkMdp.UseVisualStyleBackColor = true;
-            // 
-            // txtLogin
-            // 
-            this.txtLogin.Location = new System.Drawing.Point(10, 53);
-            this.txtLogin.Name = "txtLogin";
-            this.txtLogin.Size = new System.Drawing.Size(213, 20);
-            this.txtLogin.TabIndex = 3;
-            // 
-            // txtMdp
-            // 
-            this.txtMdp.Location = new System.Drawing.Point(10, 120);
-            this.txtMdp.Name = "txtMdp";
-            this.txtMdp.Size = new System.Drawing.Size(213, 20);
-            this.txtMdp.TabIndex = 4;
-            // 
             // btnConnect
             // 
             this.btnConnect.BackColor = System.Drawing.Color.Gray;
@@ -111,17 +68,76 @@ namespace Mediatek.vue
             this.btnConnect.Text = "CONNEXION";
             this.btnConnect.UseVisualStyleBackColor = false;
             // 
+            // txtMdp
+            // 
+            this.txtMdp.Location = new System.Drawing.Point(10, 120);
+            this.txtMdp.Name = "txtMdp";
+            this.txtMdp.Size = new System.Drawing.Size(213, 20);
+            this.txtMdp.TabIndex = 4;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(10, 53);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(213, 20);
+            this.txtLogin.TabIndex = 3;
+            // 
+            // checkMdp
+            // 
+            this.checkMdp.AutoSize = true;
+            this.checkMdp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkMdp.Location = new System.Drawing.Point(10, 146);
+            this.checkMdp.Name = "checkMdp";
+            this.checkMdp.Size = new System.Drawing.Size(185, 22);
+            this.checkMdp.TabIndex = 2;
+            this.checkMdp.Text = "Afficher le mot de passe";
+            this.checkMdp.UseVisualStyleBackColor = true;
+            // 
+            // lbMdp
+            // 
+            this.lbMdp.AutoSize = true;
+            this.lbMdp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMdp.Location = new System.Drawing.Point(6, 97);
+            this.lbMdp.Name = "lbMdp";
+            this.lbMdp.Size = new System.Drawing.Size(105, 20);
+            this.lbMdp.TabIndex = 1;
+            this.lbMdp.Text = "Mot de passe";
+            // 
+            // lbLogin
+            // 
+            this.lbLogin.AutoSize = true;
+            this.lbLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLogin.Location = new System.Drawing.Point(6, 30);
+            this.lbLogin.Name = "lbLogin";
+            this.lbLogin.Size = new System.Drawing.Size(130, 20);
+            this.lbLogin.TabIndex = 0;
+            this.lbLogin.Text = "Nom d\'utilisateur ";
+            // 
+            // picLogin
+            // 
+            this.picLogin.BackColor = System.Drawing.Color.Transparent;
+            this.picLogin.Image = global::Mediatek.Properties.Resources.imglogin;
+            this.picLogin.Location = new System.Drawing.Point(12, 35);
+            this.picLogin.MaximumSize = new System.Drawing.Size(254, 254);
+            this.picLogin.Name = "picLogin";
+            this.picLogin.Size = new System.Drawing.Size(211, 217);
+            this.picLogin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLogin.TabIndex = 1;
+            this.picLogin.TabStop = false;
+            // 
             // frmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Teal;
             this.ClientSize = new System.Drawing.Size(624, 301);
+            this.Controls.Add(this.picLogin);
             this.Controls.Add(this.groupBox1);
             this.Name = "frmLogin";
             this.Text = "Connexion";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogin)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -135,5 +151,6 @@ namespace Mediatek.vue
         private System.Windows.Forms.TextBox txtMdp;
         private System.Windows.Forms.TextBox txtLogin;
         private System.Windows.Forms.CheckBox checkMdp;
+        private System.Windows.Forms.PictureBox picLogin;
     }
 }
--- Mediatek/vue/frmLogin.cs
+++ Mediatek/vue/frmLogin.cs
@@ -16,5 +16,10 @@ namespace Mediatek.vue
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
