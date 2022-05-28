cf076e80eb61f4d6fce966dba98f4d4a0b5e7915
louise <louisepop14@gmail.com>
2022-05-28 19:40:51 +0200

    Ajout interface Connexion

--- Mediatek/vue/frmLogin.Designer.cs
+++ Mediatek/vue/frmLogin.Designer.cs
@@ -29,12 +29,111 @@ namespace Mediatek.vue
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbLogin = new System.Windows.Forms.Label();
+            this.lbMdp = new System.Windows.Forms.Label();
+            this.checkMdp = new System.Windows.Forms.CheckBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtMdp = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.txtMdp);
+            this.groupBox1.Controls.Add(this.txtLogin);
+            this.groupBox1.Controls.Add(this.checkMdp);
+            this.groupBox1.Controls.Add(this.lbMdp);
+            this.groupBox1.Controls.Add(this.lbLogin);
+            this.groupBox1.Location = new System.Drawing.Point(229, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(366, 250);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // lbLogin
+            // 
+            this.lbLogin.AutoSize = true;
+            this.lbLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLogin.Location = new System.Drawing.Point(6, 30);
+            this.lbLogin.Name = "lbLogin";
+            this.lbLogin.Size = new System.Drawing.Size(130, 20);
+            this.lbLogin.TabIndex = 0;
+            this.lbLogin.Text = "Nom d\'utilisateur ";
+            // 
+            // lbMdp
+            // 
+            this.lbMdp.AutoSize = true;
+            this.lbMdp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMdp.Location = new System.Drawing.Point(6, 97);
+            this.lbMdp.Name = "lbMdp";
+            this.lbMdp.Size = new System.Drawing.Size(105, 20);
+            this.lbMdp.TabIndex = 1;
+            this.lbMdp.Text = "Mot de passe";
+            // 
+            // checkMdp
+            // 
+            this.checkMdp.AutoSize = true;
+            this.checkMdp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkMdp.Location = new System.Drawing.Point(10, 146);
+            this.checkMdp.Name = "checkMdp";
+            this.checkMdp.Size = new System.Drawing.Size(185, 22);
+            this.checkMdp.TabIndex = 2;
+            this.checkMdp.Text = "Afficher le mot de passe";
+            this.checkMdp.UseVisualStyleBackColor = true;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(10, 53);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(213, 20);
+            this.txtLogin.TabIndex = 3;
+            // 
+            // txtMdp
+            // 
+            this.txtMdp.Location = new System.Drawing.Point(10, 120);
+            this.txtMdp.Name = "txtMdp";
+            this.txtMdp.Size = new System.Drawing.Size(213, 20);
+            this.txtMdp.TabIndex = 4;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.Gray;
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.ForeColor = System.Drawing.Color.White;
+            this.btnConnect.Location = new System.Drawing.Point(126, 190);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(223, 38);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "CONNEXION";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmLogin";
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(624, 301);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmLogin";
+            this.Text = "Connexion";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbMdp;
+        private System.Windows.Forms.Label lbLogin;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtMdp;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.CheckBox checkMdp;
     }
 }
