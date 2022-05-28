9a052db805a538798bc516a870dd73144dd498ff
louise <louisepop14@gmail.com>
2022-05-29 00:19:08 +0200

    Ajout classe AccesDonnees

---
+++ Mediatek/dal/AccesDonnees.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MediaTek.connexion;
+using MediaTek.modele;
+
+namespace Mediatek.dal
+{
+    class AccesDonnees
+    {
+        /// <summary>
+        /// Chaine de connexion à la BDD
+        /// </summary>
+        private static string connectionString = "Server=localhost;Database=mediatek;User Id=admin;Password=P@$$word; Ssl mode=none";
+
+    }
+}
