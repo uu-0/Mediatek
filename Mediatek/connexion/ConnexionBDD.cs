f2ef6aac2171d26d23226bb50cb4bd833f8bd865
louise <louisepop14@gmail.com>
2022-05-28 19:45:51 +0200

    Ajout classe connexion

---
+++ Mediatek/connexion/ConnexionBDD.cs
@@ -0,0 +1,166 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace MediaTek.connexion
+{
+    /// <summary>
+    /// Connexion à la base de données et exécution des requêtes
+    /// </summary>
+    public class ConnexionBDD
+    {
+        /// <summary>
+        /// Objet de connexion à la BDD à partir d'une chaîne de connexion
+        /// </summary>
+        private MySqlConnection connection;
+
+        /// <summary>
+        /// Constructeur privé pour créer la connexion à la BDD et l'ouvrir
+        /// </summary>
+        /// <param name="stringConnect">chaîne de connexion</param>
+        private ConnexionBDD(string stringConnect)
+        {
+            try
+            {
+                connection = new MySqlConnection(stringConnect);
+                connection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Application.Exit();
+            }
+        }
+
+        /// <summary>
+        /// Unique instance de la classe
+        /// </summary>
+        private static ConnexionBDD instance = null;
+
+        /// <summary>
+        /// Crée une instance unique de la classe
+        /// </summary>
+        /// <param name="stringConnect">chaine de connexion</param>
+        /// <returns>instance unique de la classe</returns>
+        public static ConnexionBDD GetInstance(string stringConnect)
+        {
+            if (instance is null)
+            {
+                instance = new ConnexionBDD(stringConnect);
+            }
+            return instance;
+        }
+
+        /// <summary>
+        /// Objet pour exécuter une requête SQL
+        /// </summary>
+        private MySqlCommand command;
+
+        /// <summary>
+        /// Exécuter une requête autre que "SELECT"
+        /// </summary>
+        /// <param name="stringQuery">requête autre que "SELECT"</param>
+        /// <param name="parameters"></param>
+        public void ReqUpdate(string stringQuery, Dictionary<string, object> parameters)
+        {
+            try
+            {
+                command = new MySqlCommand(stringQuery, connection);
+                foreach (KeyValuePair<string, object> parameter in parameters)
+                {
+                    command.Parameters.Add(new MySqlParameter(parameter.Key, parameter.Value));
+                }
+                command.Prepare();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Curseur objet
+        /// </summary>
+        private MySqlDataReader reader;
+
+        /// <summary>
+        /// Exécution d'une requête type "SELECT" et valorisation du curseur
+        /// </summary>
+        /// <param name="stringQuery">requête "SELECT"</param>
+        /// <param name="parameters"></param>
+        public void ReqSelect(string stringQuery, Dictionary<string, object> parameters)
+        {
+            try
+            {
+                command = new MySqlCommand(stringQuery, connection);
+                if (!(parameters is null))
+                {
+                    foreach (KeyValuePair<string, object> parameter in parameters)
+                    {
+                        command.Parameters.Add(new MySqlParameter(parameter.Key, parameter.Value));
+                    }
+                }
+                command.Prepare();
+                reader = command.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Tente de lire la ligne suivante du curseur
+        /// </summary>
+        /// <returns>false si la fin du curseur est atteint</returns>
+        public bool Read()
+        {
+            if (reader is null)
+            {
+                return false;
+            }
+            try
+            {
+                return reader.Read();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Retourne le contenu d'un champ dont le nom est passé en paramètre
+        /// </summary>
+        /// <param name="nameField">nom du champs</param>
+        /// <returns>valeur du champ</returns>
+        public object Field(string nameField)
+        {
+            if (reader is null)
+            {
+                return null;
+            }
+            try
+            {
+                return reader[nameField];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Fermeture du curseur
+        /// </summary>
+        public void Close()
+        {
+            if (!(reader is null))
+            {
+                reader.Close();
+            }
+        }
+    }
+}
